735b0e3edbdff77ef8b9d3bfe49ec4db290bac87
sayedabdulmuqsit11 <sayedabdulmuqsit11@gmail.com>
2026-04-08 15:07:46 +0530

    rename to inference.py

--- interference.py
+++ interference.py
@@ -8,7 +8,7 @@ import json
 import time
 import requests
 
-API_BASE_URL = os.environ.get("API_BASE_URL", "http://localhost:7860")
+API_BASE_URL = os.environ.get("API_BASE_URL", "https://sayedabdulmuqsit11-medi-triage-env.hf.space")
 MODEL_NAME = os.environ.get("MODEL_NAME", "gpt-4o-mini")
 HF_TOKEN = os.environ.get("HF_TOKEN", "")
 OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY", "")
